7a3586ca85e80869292d564057bfba757347ec47
Furkan <furkanyusrapamukcu@gmail.com>
2025-05-10 10:05:23 +0300

    Initial Commit

---
+++ SwiftUIMapp/SwiftUIMappApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMappApp.swift
+//  SwiftUIMapp
+//
+//  Created by Furkan Pamukcu on 10.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
